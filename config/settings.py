e6dbf65388c28287fc62c471995a9333d6d611cf
iamlakshit69 <lakshitsinghpersonal@gmail.com>
2026-03-15 11:04:28 +0530

    switched to Llama 4 Scout

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ WHISPER_DEVICE = "cpu"
 WHISPER_LANGUAGE = "en"
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-GROQ_MODEL = "llama-3.1-8b-instant"
+GROQ_MODEL        = "meta-llama/llama-4-scout-17b-16e-instruct"
 GROQ_MAX_TOKENS = 150
 GROQ_TEMPERATURE = 0.7
 
